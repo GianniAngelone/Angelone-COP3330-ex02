e8c5defbf391959dd694bcb851844c1243ee966a
GianniAngelone <angelone.gianni@gmail.com>
2021-09-12 17:21:50 -0400

    Commit

---
+++ src/com/exercise02/Exercise02.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+package com.exercise02;
+import java.util.Scanner;
+
+class Exercise02 {
+        public static void main(String[] args)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("What is input string? ");
+            String word = input.nextLine(); //Reads the next line (NAME) input.
+            int length = word.length();
+            System.out.println(String.format("%s has %d characters.", word, length));
+        }
+    }
